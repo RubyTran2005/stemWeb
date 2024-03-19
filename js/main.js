a6be216697d2205cfe461549b293a8ed82e7abe0
baongoc.tran <baongoc.tran@baongoctran.local>
2024-03-19 17:07:45 -0400

    19/3

--- js/main.js
+++ js/main.js
@@ -132,6 +132,30 @@
             }
         }
     });
-    
 })(jQuery);
 
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides1");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+      slides[i].classList.remove("fade1"); // Remove fade animation class
+    }
+    slides[slideIndex-1].style.display = "block";
+    slides[slideIndex-1].classList.add("fade1"); // Add slide animation class
+  }
